662879199607a8702e5e0a2c0db535e1a1f08adb
esj1056 <ethan.joseph@g.austincc.edu>
2020-02-06 19:26:24 -0600

    Worked on examples with switches, and did module 02 classwork. All tests succeeded.

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,15 @@
+#include "decision.h"
 //Write the function code that returns the product of hours and hourly_rate.
+
+double gross_pay(double hours, double hourly_rate)
+{
+	double result;
+	if (hours <= 40) {
+		result = hours * hourly_rate;
+	}
+	else if (hours > 40) {
+		result = hours * hourly_rate + 1.5 * (hours - 40) *hourly_rate;
+	}
+
+	return result;
+}
