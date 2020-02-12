5469c15aba1aa8633cbf2a8eb3c8ecbcba0328d0
esj1056 <ethan.joseph@g.austincc.edu>
2020-02-11 18:23:39 -0600

    while loop example

--- src/examples/03_module/01_while/main.cpp
+++ src/examples/03_module/01_while/main.cpp
@@ -1,7 +1,17 @@
 #include "while.h"
+#include <iostream>
+
+using std::cin; using std::cout;
 
 int main() 
 {
+	int num, sum;
+	cout << "Enter a number: ";
+	cin >> num;
+
+	sum = sum_of_squares(num);
+
+	cout << "Sum of squares for " << num << " is " << sum;
 
 	return 0;
 }
