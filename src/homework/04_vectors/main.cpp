0e1ca49d5ec41f843bc13e82e52fc9f55d0f8e2d
esj1056 <ethan.joseph2@g.austincc.edu>
2020-02-28 22:24:59 -0600

    Completed homework 4 to the best of my ability.

--- src/homework/04_vectors/main.cpp
+++ src/homework/04_vectors/main.cpp
@@ -1,3 +1,7 @@
+#include "vectors.h"
+#include <iostream>
+
+using std::cout; using std::cin;
 /*
 use a vector of int with values 8, 4, 20, 88, 66, 99
 
@@ -9,6 +13,29 @@ Program continues until user decides to exit.
 */
 int main() 
 {
+	auto decision1 {0};
+	auto decision2 = "";
+	auto maximum{0};
+	auto usernumber{0};
+	vector<int> vectorset {0}
+	vector<int> vector1{ 8,4,20,88,66,99 };
+	cout << "Input 1 to recieve the maximum number of a predetermined set." << "\n" << "Press 2 in order to get a list of all primes counting down from a given number." << "\n" << "Please make your selection now." << "\n";
+	cin >> decision1;
+
+	do{
+		if (decision1 == 1){
+			maximum = get_max_from_vector(vector1);
+			cout << "The maximum number for the vector is: " << maximum << "\n";
+		}
+		else if (decision1 == 2){
+			cout << "Please enter any whole number:" << "\n"; 
+			cin >> usernumber;
+			vectorset = vector_of_primes(usernumber);
+			cout << "Every prime counting down from this number is: " << vectorset << "\n";
+		}
+		cout << "Please press y to go through this process again, and any other key to exit: ";
+		cin >> decision2;
+	}while (decision2 == "y")
 
 	return 0;
 }
--- test/homework_test/04_vectors_test/04_vectors_tests.cpp
+++ test/homework_test/04_vectors_test/04_vectors_tests.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-/*TEST_CASE("Testing Get Max From Vector"){
+TEST_CASE("Testing Get Max From Vector"){
 	vector<int> integers{ 3,8,1,99,1000 };
 	int max = get_max_from_vector(integers);
 	REQUIRE(max == 1000);
@@ -21,5 +21,16 @@ TEST_CASE("Verify Test Configuration", "verification") {
 }
 
 TEST_CASE("Testing Is Prime") {
+	REQUIRE(isprime(2) == true);
+	REQUIRE(isprime(4) == false);
+	REQUIRE(isprime(43) == true);
+	REQUIRE(isprime(44) == false);
+}
 
-}*/
+TEST_CASE("Testing Vector of Primes"){
+	vector<int> testvector1{ 2,3,5,7 };
+	REQUIRE(vector_of_primes(10) == testvector1);
+
+	vector<int> testvector2{ 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47 };
+	REQUIRE(vector_of_primes(50) == testvector2);
+}
